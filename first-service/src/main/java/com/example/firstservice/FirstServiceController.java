0c0df7596603e35170ebbe1c7832cf11d7b3aa33
goodlucky1215 <goodlucky1215@naver.com>
2021-08-30 22:01:48 +0900

    JPA추가

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,27 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the first service";
@@ -22,7 +34,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "안녕, 이건 : message to the first service";
+    public String check(HttpServletRequest request){
+        log.info("Server Port={}", request.getServerPort());
+        return String.format("안녕, 이건 : message to the first service Port %s",env.getProperty("local.server.port"));
     }
 }

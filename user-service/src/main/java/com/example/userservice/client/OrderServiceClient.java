96783a9aeac0f117a9a18c9cc00fb6c0dfd2211b
goodlucky1215 <goodlucky1215@naver.com>
2021-10-13 22:44:40 +0900

    2개의 order-service를 구동시 database문제

--- user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -10,6 +10,6 @@ import java.util.List;
 @FeignClient(name="order-service") //name="order-service"은 microservice name을 명시해야함
 public interface OrderServiceClient {
 
-    @GetMapping("/order-service/{userId}/orders_ng") //0rder-service에서 가져올 path경로를 넣어주면 됨.
+    @GetMapping("/order-service/{userId}/orders") //0rder-service에서 가져올 path경로를 넣어주면 됨.
     List<ResponseOrder> getOrders(@PathVariable String userId); //path의 가변 변수(유저의 아이디)
 }

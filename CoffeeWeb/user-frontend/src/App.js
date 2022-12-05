2377947bb8bc9a953f9e2b9216cb88a51ee9311b
goodlucky1215 <goodlucky1215@naver.com>
2022-12-05 23:52:27 +0900

    .

--- CoffeeWeb/user-frontend/src/App.js
+++ CoffeeWeb/user-frontend/src/App.js
@@ -1,19 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
-  const [hello, setHello] = useState('')
+  return(
+    <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">로그인</Link>
+            </li>
+            <li>
+              <Link to="/join">회원가입</Link>
+            </li>
+            <li>
+              <Link to="/Order">주문</Link>
+            </li>
+            <li>
+              <Link to="/UserInfo">사용자 정보</Link>
+            </li>
+          </ul>
 
-  useEffect(() => {
-    axios.get('/user-service/hello')
-        .then(response => setHello(response.data))
-        .catch(error => console.log(error))
-  }, []);
-
-  return (
-      <div>
-        백엔드에서 가져온 데이터입니다 : {hello}
-      </div>
+          <switch>
+            <Route path={"/"} exact={true}>
+                <Login />
+            </Route>
+            <Route path={"/join"} exact={true}>
+                <Join />
+            </Route>
+            <Route path={"/Order"} exact={true}>
+                <Order />
+            </Route>
+            <Route path={"/UserInfo"} exact={true}>
+                <UserInfo />
+            </Route>
+          </switch>
+        </div>
+    </BrowserRouter>
   );
 }
 
--- CoffeeWeb/user-frontend/src/setupProxy.js
+++ CoffeeWeb/user-frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/user-service/hello',
+        '/user-service',
         createProxyMiddleware({
             target: 'http://localhost:9600',// 서버 URL or localhost:설정한포트번호
             changeOrigin: true,

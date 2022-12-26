0b7fd88ccf7ed44798779b54ee4c393512fc0355
goodlucky1215 <goodlucky1215@naver.com>
2022-12-27 00:14:34 +0900

    orde-service repository making

--- FashionWeb/order-service/src/main/java/fashion/orderservice/repository/OrderRepository.java
+++ FashionWeb/order-service/src/main/java/fashion/orderservice/repository/OrderRepository.java
@@ -1,5 +1,18 @@
 package fashion.orderservice.repository;
 
+import fashion.orderservice.entity.Item;
+import fashion.orderservice.entity.Order;
+
+import java.util.List;
+
 public interface OrderRepository {
+    //상품 목록
+    List<Item> findAll();
+
+    //상품 주문
+    void save(Order order);
+
+    //주문 목록 조회
+    List<Order> findbyPkId(Long pkId);
 
 }
--- FashionWeb/order-service/src/main/java/fashion/orderservice/repository/OrderRepositoryImpl.java
+++ FashionWeb/order-service/src/main/java/fashion/orderservice/repository/OrderRepositoryImpl.java
@@ -1,7 +1,33 @@
 package fashion.orderservice.repository;
 
+import fashion.orderservice.entity.Item;
+import fashion.orderservice.entity.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Repository
 public class OrderRepositoryImpl implements OrderRepository {
+
+    final private EntityManager em;
+
+    @Override
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i",Item.class).getResultList();
+    }
+
+    @Override
+    public void save(Order order) {
+        em.persist(order);
+        em.persist(order.getOrderItems());
+    }
+
+    @Override
+    public List<Order> findbyPkId(Long pkId) {
+        return em.createQuery("select o from Order o where o.member.pkId=:pkId",Order.class)
+                .setParameter("pkId",pkId).getResultList();
+    }
 }

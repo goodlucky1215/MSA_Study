e3262641c2ff1fdbe0e419b9881be85ec2c959f8
goodlucky1215 <goodlucky1215@naver.com>
2023-01-06 23:20:49 +0900

    controller aksema

--- FashionWeb/order-service/src/main/java/fashion/orderservice/controller/OrderController.java
+++ FashionWeb/order-service/src/main/java/fashion/orderservice/controller/OrderController.java
@@ -1,15 +1,47 @@
 package fashion.orderservice.controller;
 
+import fashion.orderservice.dto.ItemDto;
+import fashion.orderservice.dto.MemberOrdersDto;
+import fashion.orderservice.dto.OrderitemDto;
+import fashion.orderservice.service.OrderService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RestController
 @RequestMapping("/order-service")
+@RequiredArgsConstructor
 public class OrderController {
+
+    final private OrderService orderService;
 
     @GetMapping("hello")
     public String hello(){
         return "hello orderservice";
     }
+
+    @GetMapping("itemList")
+    private List<ItemDto> getItemList(){
+        return orderService.getItemList();
+    }
+
+    //상품 주문
+    @PostMapping("order")
+    private void saveOrderItems(HttpServletRequest request, List<OrderitemDto> orderItems) {
+        orderService.saveOrderItems(Long.parseLong(request.getHeader("pkId")), orderItems);
+        return "redirect"
+    }
+
+    //주문 목록 조회
+    @GetMapping("orderList")
+    private List<MemberOrdersDto> findMemberOrderList(HttpServletRequest request{
+        return orderService.findMemberOrderList(Long.parseLong(request.getHeader("pkId")));
+    }
+
 }

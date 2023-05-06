1958ea08e8f86326a03c4fe9d3fe5d8e58337815
jiny <goodlucky1215@naver.com>
2023-05-06 13:01:26 +0900

    seller-service jar make

--- FashionWeb/seller-service/src/main/java/fashion/sellerservice/SellerserviceApplication.java
+++ FashionWeb/seller-service/src/main/java/fashion/sellerservice/SellerserviceApplication.java
@@ -1,13 +1,47 @@
 package fashion.sellerservice;
 
+import com.netflix.discovery.converters.Auto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SellerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellerServiceApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+
+    @Autowired
+    EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em){
+        return new JPAQueryFactory(em);
+    }
 }
